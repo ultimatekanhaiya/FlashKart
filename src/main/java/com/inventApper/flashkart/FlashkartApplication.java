e11d8ad112393a1d25d3c6cd037298d820b0add8
ultimatekanhaiya <ultimatekanhaiya@gmail.com>
2024-04-03 19:07:49 +0530

    project setup

---
+++ src/main/java/com/inventApper/flashkart/FlashkartApplication.java
@@ -0,0 +1,13 @@
+package com.inventApper.flashkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventApper/flashkart/FlashkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventApper.flashkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
